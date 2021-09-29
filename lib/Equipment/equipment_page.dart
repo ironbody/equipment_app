be70d02a52d655f9f008e9ee3fe4faf9d02af7df
Simon Saberian <simon.saberian@gmail.com>
2021-09-29 16:55:03 +0200

    added FAB to equipment page

--- lib/Equipment/equipment_page.dart
+++ lib/Equipment/equipment_page.dart
@@ -1,10 +1,13 @@
-import 'package:equipment_app/Equipment/equipment.dart';
+import 'package:equipment_app/models/equipment.dart';
+import 'package:equipment_app/Equipment/equipment_form.dart';
 import 'package:equipment_app/Equipment/equipment_listview.dart';
 import 'package:equipment_app/db/database_provider.dart';
 import 'package:flutter/material.dart';
 
 class EquipmentPage extends StatefulWidget {
-  const EquipmentPage({Key? key,}) : super(key: key);
+  const EquipmentPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   _EquipmentPageState createState() => _EquipmentPageState();
@@ -13,36 +16,46 @@ class EquipmentPage extends StatefulWidget {
 class _EquipmentPageState extends State<EquipmentPage> {
   DatabaseProvider db = DatabaseProvider();
 
+  void goToForm(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return const EquipmentForm();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Equipment List")),
-      body: FutureBuilder(
-        future: db.getEquipment(),
-        builder: (BuildContext context, AsyncSnapshot<List<Equipment>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-                child: SizedBox(
-                    width: 30.0,
-                    height: 30.0,
-                    child: CircularProgressIndicator()));
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasError) {
-              return Text("Error occurred: ${snapshot.error}");
+        appBar: AppBar(title: const Text("Equipment List")),
+        body: FutureBuilder(
+          future: db.getEquipment(),
+          builder:
+              (BuildContext context, AsyncSnapshot<List<Equipment>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                  child: SizedBox(
+                      width: 30.0,
+                      height: 30.0,
+                      child: CircularProgressIndicator()));
             }
-            if (snapshot.hasData) {
-              final _equipmentList = snapshot.data ?? [];
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: EquipmentListView(equipmentList: _equipmentList,),
-              );
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return Text("Error occurred: ${snapshot.error}");
+              }
+              if (snapshot.hasData) {
+                final _equipmentList = snapshot.data ?? [];
+                return EquipmentListView(
+                  equipmentList: _equipmentList,
+                );
+              }
             }
-          }
-    
-          return Container();
-        },
-      ),
-    );
+
+            return Container();
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => goToForm(context),
+          tooltip: "Add Equipment",
+          child: const Icon(Icons.add),
+        ));
   }
 }
