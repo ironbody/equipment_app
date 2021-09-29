c5b17017c735753a467461a377c173a03a5b3017
Simon Saberian <simon.saberian@gmail.com>
2021-09-29 16:52:24 +0200

    moved and updated equipment file

---
+++ lib/models/equipment.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+
+enum DeviceType {
+  laptop,
+  tablet,
+  mouse,
+  keyboard,
+  headphones,
+  camera,
+  other,
+}
+
+extension DeviceTypeExtension on DeviceType {
+  String toShortString() {
+    return toString().split('.').last;
+  }
+}
+
+DeviceType stringToDeviceType(String str) {
+  return DeviceType.values.firstWhere(
+      (e) => e.toString() == 'DeviceType.' + str,
+      orElse: () => DeviceType.other);
+}
+
+class Equipment {
+  Equipment({
+    this.id,
+    required this.name,
+    required this.description,
+    required this.serial,
+    required this.deviceType,
+    required this.duration,
+    this.startDate,
+    this.endDate,
+    this.available,
+  });
+
+  int? id;
+  String name;
+  String description;
+  String serial;
+  DeviceType deviceType;
+  int duration;
+  DateTime? startDate;
+  DateTime? endDate;
+  bool? available;
+  
+
+  factory Equipment.fromMap(Map<String, dynamic> json) => Equipment(
+        id: json["id"],
+        name: json["name"],
+        description: json["description"],
+        serial: json["serial"],
+        deviceType: json["deviceType"],
+        bookingDate: DateTime.parse(json["bookingDate"]),
+        duration: json["duration"],
+        available: json["available"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+        "description": description,
+        "serial": serial,
+        "deviceType": deviceType.toShortString(),
+        "bookingDate": bookingDate?.toIso8601String(),
+        "duration": duration,
+        "available": available ? 1 : 0,
+      };
+}
