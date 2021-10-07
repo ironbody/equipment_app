9efafdd620e3e398c08dd1de59eec41c2c6e754a
Simon Saberian <simon.saberian@gmail.com>
2021-10-07 13:15:52 +0200

    Added description widget

---
+++ lib/widgets/details_page/details_description.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class DetailsDescription extends StatelessWidget {
+  const DetailsDescription({Key? key, required this.description})
+      : super(key: key);
+
+  final String description;
+  final double fontSize = 20.0;
+  final double titleFontSize = 24.0;
+  final double padding = 8.0;
+  final double minHeight = 80.0;
+  final double maxHeight = 320.0;
+  final double width = 360.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        minHeight: minHeight,
+        maxHeight: maxHeight,
+        minWidth: width,
+        maxWidth: width,
+      ),
+      child: Card(
+          child: Padding(
+              padding: EdgeInsets.all(padding),
+              child: Column(
+                children: [
+                  Text(
+                    "Description",
+                    style: TextStyle(fontSize: titleFontSize),
+                  ),
+                  const SizedBox(height: 6.0,),
+                  Expanded(
+                      child: SingleChildScrollView(
+                          child: Text(description,
+                              style: TextStyle(
+                                fontSize: fontSize,
+                              )))),
+                ],
+              ))),
+    );
+    // return SizedBox(
+    //   height: height,
+    //   child: Padding(
+    //     padding: EdgeInsets.all(padding),
+    //     child: Expanded(
+    //         child: SingleChildScrollView(
+    //             child: Text(description,
+    //                 style: TextStyle(
+    //                   fontSize: fontSize,
+    //                 )))),
+    //   ),
+    // );
+  }
+}
