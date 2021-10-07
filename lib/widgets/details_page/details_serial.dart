9ff4e96298e55fbb5f26e6271c49674b41f189da
Simon Saberian <simon.saberian@gmail.com>
2021-10-07 13:16:00 +0200

    Added serial number widget

---
+++ lib/widgets/details_page/details_serial.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class DetailsSerial extends StatelessWidget {
+  const DetailsSerial({Key? key, required this.serial}) : super(key: key);
+
+  final String serial;
+  final double fontSize = 20.0;
+  final double titleFontSize = 24.0;
+  final double padding = 8.0;
+  final double minHeight = 80.0;
+  final double maxHeight = 300.0;
+  final double width = 360.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        // minHeight: minHeight,
+        maxHeight: maxHeight,
+        minWidth: width,
+        maxWidth: width,
+      ),
+      child: Card(
+          child: Padding(
+              padding: EdgeInsets.all(padding),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Serial Number",
+                    style: TextStyle(fontSize: titleFontSize),
+                  ),
+                  const SizedBox(
+                    height: 6.0,
+                  ),
+                  SingleChildScrollView(
+                      child: Text(serial,
+                          style: TextStyle(
+                            fontSize: fontSize,
+                          ))),
+                ],
+              ))),
+    );
+    // return SizedBox(
+    //   height: height,
+    //   child: Padding(
+    //     padding: EdgeInsets.all(padding),
+    //     child: Expanded(
+    //         child: SingleChildScrollView(
+    //             child: Text(description,
+    //                 style: TextStyle(
+    //                   fontSize: fontSize,
+    //                 )))),
+    //   ),
+    // );
+  }
+}
